da0057d6fca2a0c45f6aad696a0b9d827c726c00
mayur.c <mayur.c@position2.com>
2025-08-26 12:17:32 +0530

    default to light theme and middleware changes

--- apps/sim/app/theme-provider.tsx
+++ apps/sim/app/theme-provider.tsx
@@ -7,8 +7,9 @@ export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return (
     <NextThemesProvider
       attribute='class'
-      defaultTheme='system'
-      enableSystem
+      defaultTheme='light'
+      value={{ light: 'light' }} // 👈 Restrict themes to only light
+      enableSystem={false}
       disableTransitionOnChange
       storageKey='sim-theme'
       {...props}
