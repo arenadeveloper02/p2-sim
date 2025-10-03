557dbfb2de8421d76888b6776e99301e4327da96
arenadeveloper02 <arenadeveloper@position2.com>
2025-10-03 17:34:26 +0530

    figma new files addition

--- apps/sim/tools/figma/index.ts
+++ apps/sim/tools/figma/index.ts
@@ -1,6 +1,8 @@
 import { convertFigmaTool } from './convert_figma'
 import { createFigmaTool } from './create_figma'
+import { createStylesVariablesTool } from './create_styles_variables'
 import { deleteCommentTool } from './delete_comment'
+import { figmaMakeIntegrationTool } from './figma_make_integration'
 import { getCommentsTool } from './get_comments'
 import { getFileTool } from './get_file'
 import { getFileImagesTool } from './get_file_images'
@@ -8,9 +10,13 @@ import { getFileNodesTool } from './get_file_nodes'
 import { getProjectFilesTool } from './get_project_files'
 import { getTeamProjectsTool } from './get_team_projects'
 import { postCommentTool } from './post_comment'
+import { wireframeToUITool } from './wireframe_to_ui'
 
 export const figmaCreateTool = createFigmaTool
 export const figmaConvertTool = convertFigmaTool
+export const figmaCreateStylesVariablesTool = createStylesVariablesTool
+export { figmaMakeIntegrationTool }
+export { wireframeToUITool as figmaWireframeToUITool }
 export const figmaGetCommentsTool = getCommentsTool
 export const figmaPostCommentTool = postCommentTool
 export const figmaDeleteCommentTool = deleteCommentTool
