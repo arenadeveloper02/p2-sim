9dd2924384faeb6f142e91aa69d234767014a407
arenadeveloper02 <arenadeveloper@position2.com>
2025-11-20 16:57:14 +0530

    chanegs

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,9 +1,12 @@
-#!/bin/sh
-set -e
+# 1. Install Xvfb (if not already)
+apk add --no-cache xvfb
 
-# Start virtual display
+# 2. Start Xvfb on display :99
 Xvfb :99 -screen 0 1920x1080x24 -ac +extension GLX +render -noreset &
+
+# 3. Export DISPLAY so Chromium/Selenium can see it
 export DISPLAY=:99
 
-# Now start your app (Next.js + Bun)
-exec "$@"
+# 4. Confirm
+echo $DISPLAY
+# should print: :99
