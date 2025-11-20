aa88c9d93a7cb3f43e188af4d7f6521329f1a0de
Saiteja Sankoji <saiteja.s@Saiteja-MBA.local>
2025-11-20 16:41:00 +0530

    change the theme

--- apps/sim/lib/branding/branding.ts
+++ apps/sim/lib/branding/branding.ts
@@ -12,6 +12,7 @@ export interface ThemeColors {
 export interface BrandConfig {
   name: string
   logoUrl?: string
+  logoUrlBlacktext?: string
   faviconUrl?: string
   customCssUrl?: string
   supportEmail?: string
@@ -27,6 +28,8 @@ export interface BrandConfig {
 const defaultConfig: BrandConfig = {
   name: 'Agentic AI',
   logoUrl: 'https://arenav2image.s3.us-west-1.amazonaws.com/arena_svg_white.svg',
+  logoUrlBlacktext:
+    'https://arenav2image.s3.us-west-1.amazonaws.com/rt/calibrate/Arena_Logo_WebDashboard.svg',
   faviconUrl: '/sim.svg',
   customCssUrl: undefined,
   supportEmail: 'arenadeveloper@position2.com',
@@ -66,6 +69,7 @@ export const getBrandConfig = (): BrandConfig => {
   return {
     name: getEnv('NEXT_PUBLIC_BRAND_NAME') || defaultConfig.name,
     logoUrl: getEnv('NEXT_PUBLIC_BRAND_LOGO_URL') || defaultConfig.logoUrl,
+    logoUrlBlacktext: getEnv('NEXT_PUBLIC_BRAND_LOGO_URL_DARK') || defaultConfig.logoUrlBlacktext,
     faviconUrl: getEnv('NEXT_PUBLIC_BRAND_FAVICON_URL') || defaultConfig.faviconUrl,
     customCssUrl: getEnv('NEXT_PUBLIC_CUSTOM_CSS_URL') || defaultConfig.customCssUrl,
     supportEmail: getEnv('NEXT_PUBLIC_SUPPORT_EMAIL') || defaultConfig.supportEmail,
