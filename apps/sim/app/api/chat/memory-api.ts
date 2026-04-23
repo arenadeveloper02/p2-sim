e049054c90cfc78230f8c103d7437edc323a63ca
arenadeveloper02 <arenadeveloper@position2.com>
2026-04-23 18:45:13 +0530

    changes for bug

--- apps/sim/app/api/chat/memory-api.ts
+++ apps/sim/app/api/chat/memory-api.ts
@@ -1,6 +1,6 @@
 import { createLogger } from '@sim/logger'
 import { env } from '@/lib/core/config/env'
-import { generateId } from '@/lib/core/utils/uuid'
+import { generateId } from '@sim/utils/id'
 
 const logger = createLogger('MemoryAPI')
 
