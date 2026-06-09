0b902486d8cdc48e9833c0f66e5d91d4f5c2f511
mayur.c <mayur.c@position2.com>
2026-06-09 15:12:58 +0530

    added users

--- apps/sim/tools/google_slides/get_p2_users.ts
+++ apps/sim/tools/google_slides/get_p2_users.ts
@@ -18,106 +18,113 @@ interface GetP2UsersResponse {
   }
 }
 
+const BASE_URL = 'https://arenav2image.s3.us-west-1.amazonaws.com/presentation-profile-images'
+
 const TEAM_MEMBERS: P2User[] = [
   {
     name: 'Rajiv Parikh',
     designation: 'CEO',
-    url: 'https://arenav2image.s3.us-west-1.amazonaws.com/presentation-profile-images/RajivParikh.png',
+    url: `${BASE_URL}/Rajiv%20Parikh.jpg`,
   },
   {
     name: 'Vikrant',
     designation: 'Chief Technology Officer, Product',
-    url: 'https://arenav2image.s3.us-west-1.amazonaws.com/presentation-profile-images/Vikrant.jpeg',
+    url: `${BASE_URL}/Vikrant.jpg`,
   },
   {
     name: 'Sajjan Kanukolanu',
     designation: 'VP, Global Operations',
-    url: 'https://arenav2image.s3.us-west-1.amazonaws.com/presentation-profile-images/SajjanK.png',
+    url: `${BASE_URL}/Sajjan%20Kanukolanu.jpg`,
   },
   {
     name: 'Sanjiv Parikh',
     designation: 'VP, Client Growth',
-    url: 'https://arenav2image.s3.us-west-1.amazonaws.com/presentation-profile-images/SanjivParikh.png',
+    url: `${BASE_URL}/Sanjiv%20Parikh.jpg`,
   },
   {
     name: 'Kumar G.',
     designation: 'Board Member, Founder, Vxtel & Virident',
-    url: 'https://arenav2image.s3.us-west-1.amazonaws.com/presentation-profile-images/KumarG.jpeg',
+    url: `${BASE_URL}/Kumar%20G.jpg`,
   },
   {
     name: 'Shay Phillips',
     designation: 'Board Member, AT&T Exec',
-    url: 'https://arenav2image.s3.us-west-1.amazonaws.com/presentation-profile-images/ShayPhillips.png',
+    url: `${BASE_URL}/Shay%20Phillips.jpg`,
   },
   {
     name: 'Mitsy Lopez Baranello',
     designation: 'Board Member, Huge (IPG Agency)',
-    url: 'https://arenav2image.s3.us-west-1.amazonaws.com/presentation-profile-images/MitzyLopez.png',
+    url: `${BASE_URL}/Mitsy%20Lopez%20Baranello.jpg`,
   },
   {
     name: 'Jon Miller',
     designation: 'Board Member',
-    url: 'https://arenav2image.s3.us-west-1.amazonaws.com/presentation-profile-images/JonMiller.jpeg',
+    url: `${BASE_URL}/Jon%20Miller.jpg`,
   },
   {
     name: 'Jason',
     designation: 'Creative Director',
-    url: 'https://arenav2image.s3.us-west-1.amazonaws.com/presentation-profile-images/Jason.png',
+    url: `${BASE_URL}/Jason.jpg`,
   },
   {
     name: 'Ahit',
     designation: 'Copy Director',
-    url: 'https://arenav2image.s3.us-west-1.amazonaws.com/presentation-profile-images/Ahit.jpeg',
+    url: `${BASE_URL}/Ahit.jpg`,
   },
   {
     name: 'Bharadwaj',
     designation: 'Creative Supervisor',
-    url: 'https://arenav2image.s3.us-west-1.amazonaws.com/presentation-profile-images/Bharadwaj.jpeg',
+    url: `${BASE_URL}/Bharadwaj.jpg`,
   },
   {
     name: 'Kiran',
     designation: 'Senior VFX Producer',
-    url: 'https://arenav2image.s3.us-west-1.amazonaws.com/presentation-profile-images/Kiran.jpeg',
+    url: `${BASE_URL}/Kiran.jpg`,
   },
   {
     name: 'Niketh',
     designation: 'Graphic Designer',
-    url: 'https://arenav2image.s3.us-west-1.amazonaws.com/presentation-profile-images/Niketh.jpeg',
+    url: `${BASE_URL}/Niketh.jpg`,
   },
   {
     name: 'Rajesh',
     designation: 'Senior Director, Experience Design',
-    url: 'https://arenav2image.s3.us-west-1.amazonaws.com/presentation-profile-images/Rajesh.jpeg',
+    url: `${BASE_URL}/RajeshM.jpg`,
+  },
+  {
+    name: 'Seetha',
+    designation: 'HR Administrator',
+    url: `${BASE_URL}/Seetha.jpg`,
   },
   {
     name: 'Vikram',
     designation: 'VP, Computing Systems Marketing',
-    url: 'https://arenav2image.s3.us-west-1.amazonaws.com/presentation-profile-images/Vikram.jpeg',
+    url: `${BASE_URL}/Vikram.jpg`,
   },
   {
     name: 'Brijesh',
     designation: 'Senior Director, Operations Excellence',
-    url: 'https://arenav2image.s3.us-west-1.amazonaws.com/presentation-profile-images/Brijesh.jpeg',
+    url: `${BASE_URL}/Brijesh.jpg`,
   },
   {
     name: 'Sudheer',
     designation: 'Senior Director, PA',
-    url: 'https://arenav2image.s3.us-west-1.amazonaws.com/presentation-profile-images/Sudheer.jpeg',
+    url: `${BASE_URL}/Sudheer.jpg`,
   },
   {
     name: 'Sangeetha',
     designation: 'Finance Controller, Finance',
-    url: 'https://arenav2image.s3.us-west-1.amazonaws.com/presentation-profile-images/Sangeetha.jpeg',
+    url: `${BASE_URL}/Sangeetha.jpg`,
   },
   {
     name: 'Bujji Babu',
     designation: 'Senior Director, Media & Affiliate Partnership',
-    url: 'https://arenav2image.s3.us-west-1.amazonaws.com/presentation-profile-images/BujjiBabu.jpeg',
+    url: `${BASE_URL}/Bujji%20Babu.jpg`,
   },
   {
     name: 'Mohan A',
     designation: 'Senior Director, HR',
-    url: 'https://arenav2image.s3.us-west-1.amazonaws.com/presentation-profile-images/MohanA.jpeg',
+    url: `${BASE_URL}/Mohan%20A.jpg`,
   },
 ]
 
