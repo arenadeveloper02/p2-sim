c487ad1d103a99af54d0f197bfdebfb83e58cd05
Saiteja Sankoji <saiteja.s@Saiteja-MBA.local>
2026-06-30 18:53:29 +0530

    build check

--- packages/emcn/src/components/code/prism.ts
+++ packages/emcn/src/components/code/prism.ts
@@ -2,6 +2,11 @@ import { highlight, languages } from 'prismjs'
 import 'prismjs/components/prism-javascript'
 import 'prismjs/components/prism-python'
 import 'prismjs/components/prism-json'
+import 'prismjs/components/prism-typescript'
+import 'prismjs/components/prism-jsx'
+import 'prismjs/components/prism-tsx'
+import 'prismjs/components/prism-bash'
+import 'prismjs/components/prism-yaml'
 
 /**
  * Prism.js highlighting utilities isolated in a dedicated module.
